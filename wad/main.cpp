a0eb541c011a2fc6ea3cb5106c480f6b9829aef1
Amir Masri <masriamir@gmail.com>
2020-10-02 10:40:27 -0400

    disable synchronization between iostreams and printf-style functions as per CppSL.io.10 of the CPP Core Guidelines

--- wad/main.cpp
+++ wad/main.cpp
@@ -2,6 +2,8 @@
 
 int main(const int argc, const char* argv[])
 {
+    std::ios_base::sync_with_stdio(false);
+
     if (argc > 1) {
         try {
             wad::Wad_file wad_file{std::string{argv[1]}};
