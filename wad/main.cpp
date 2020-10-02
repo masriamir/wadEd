1f7f443424d0a1eb9bd021e8fd983ed7e6e1c88c
Amir Masri <masriamir@gmail.com>
2020-10-02 13:33:50 -0400

    add 'const' qualifier

--- wad/main.cpp
+++ wad/main.cpp
@@ -8,7 +8,7 @@ int main(const int argc, const char* argv[])
 
     if (argc > 1) {
         try {
-            wad::Wad_file wad_file{std::string{argv[1]}};
+            const wad::Wad_file wad_file{std::string{argv[1]}};
 
             cout << "loaded " << wad_file.filename() << '\n';
         }
